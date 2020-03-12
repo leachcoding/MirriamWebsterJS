8d4d17e34f65b9eb1d8b1c673c47eaacb0ceb1f6
Jay Leach <leachcoding@gmail.com>
2020-03-12 18:45:59 -0400

    Solution finished

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // STEP 1: IN THIS ASSIGNMENT, THINK ABOUT THE RELATIONSHIP BETWEEN HTML AND JS FOR YOUR SEARCH. LOOK INSIDE OF YOUR HTML FILE AS WELL FOR THIS -- YOU WILL NEED A FUNCTION THAT WILL HANDLE YOUR CHANGE EVENT AS A TIP. INSIDE OF THIS SEARCH FUNCTION, YOU WILL NEED TO CREATE A CARD(S) THAT WILL DISPLAY THE SEARCH TERM YOU PROVIDED THROUGH THE MIRRIAM WEBSTER COLLEGIATE DICTIONARY API. LETS DO THIS!!!!
 function searchWord(val) {
   function dictionaryCard(data) {
+
     function checkIfNull(str) {
       if(str) {
         return str;
@@ -14,6 +15,7 @@ function searchWord(val) {
       newStems = document.createElement('p'),
       newShortDef = document.createElement('p'),
       newDefDate = document.createElement('p')
+
     // Set your content
     newId.textContent = `Id: ${data.meta.id}`;
     newSrc.textContent = `Source: ${data.meta.src}`;
@@ -30,6 +32,7 @@ function searchWord(val) {
 
     // Apply any styles or classes HERE
     newCard.classList.add('card');
+
 
     // Event handlers
     return newCard;
@@ -64,14 +67,58 @@ function removeCard() {
   elements.forEach(el => el.remove());
 }
 
-// STEP 2: USE AXIOS AND SEND A GET REQUEST TO THE FOLLOWING:
+function searchSpanishWord(val) {
+  function spanishCard(data) {
+    function checkIfNull(str) {
+      if(str) {
+        return str;
+      }
+      return '';
+    }
 
-// https://www.dictionaryapi.com/api/v3/references/collegiate/json/volume?key=blah
+    //Create Elements
+    const newCard = document.createElement('div'),
+      newId = document.createElement('p'),
+      newSrc = document.createElement('p'),
+      newStems = document.createElement('p'),
+      newShortDef = document.createElement('p')
+    // Set your content
+    newId.textContent = `Id: ${data.meta.id}`;
+    newSrc.textContent = `Source: ${data.meta.src}`;
+    newStems.textContent = `Stems: ${data.meta.stems.map(element => element)}`;
+    newShortDef.textContent = `Definitions: ${data.shortdef.map(element => ' ' + element)}`;
+    // Create the structure and append to the DOM
+    newCard.appendChild(newId);
+    newCard.appendChild(newSrc);
+    newCard.appendChild(newStems);
+    newCard.appendChild(newShortDef);
+    // Apply styles and classes here
+    newCard.classList.add('spanishCard');
+    // Event Handler
+    return newCard;
+  }
 
-// where 'volume' is the search term word you are providing and where 'blah' is the api key provided to you on signup for the api.
+  const spanishCards = document.querySelector('.spanishCards');
+  let spanishCardInfo = [];
 
-// STEP 3: CREATE A FUNCTION THAT WILL ACCEPT THIS DATA FROM THE API AND CREATE A COMPONENT THAT RETURNS A DOM ELEMENT STRUCTURE OF YOUR CHOOSING FROM THE DATA:
+  axios
+    .get(`https://www.dictionaryapi.com/api/v3/references/spanish/json/${val}?key=94262e67-155d-4509-839a-896e030e5316`)
+    .then(response => {
+      console.log(response, "SPANISH RESPONSE");
+      console.log(response.data, "SPANISH RESPONSE.DATA");
+      response.data.forEach(element => {
+        spanishCardInfo.push(element);
+      })
+      spanishCardInfo.forEach(element => {
+        spanishCards.appendChild(spanishCard(element));
+      })
+    })
+    .catch(error => {
+      console.log('the data was not returned', error);
+    });
+}
 
-// STEP 4: PASS THE DATA, CREATING A NEW COMPONENT FOR EACH WORD, AND ADD IT TO THE DOM
-
-// STEP 5: NOW THAT THIS COMPONENT IS HERE, MAKE ANOTHER COMPONENT FOR THE TRANSLATIONS AND ITS SEARCH FUNCTION. MAKE AN APPROPRIATE GET REQUEST FROM THE SPANISH DICTIONARY API. IF YOU ARE UNSURE HOW TO FIND THIS LOOK AT THE DOCUMENTATION FOR THE API. ADD THIS COMPONENT TO THE DOM AS WELL.
+function removeSpanishCard() {
+  let elements = document.querySelectorAll('.spanishCard');
+  elements.forEach(el => el.remove());
+}
